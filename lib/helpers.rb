e27319b3a497db107622be93fd5675cf1c6b674c
Cameron Higby-Naquin <cameron.higbynaquin@gmail.com>
2013-08-30 17:24:47 -0400

    Formatting fixes.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -68,7 +68,7 @@ end
 
 # should this include "by" between title and author??
 def reading_nav_link(reading)
-  title = link_to(reading[:title].gsub("'", '&#8217;'), reading.path)
+  title = link_to(reading[:title].gsub("'", '&#8217;'), reading.path, :class => 'reading')
   author = reading[:author].gsub("'", '&#8217;')
   date = reading[:created_at].strftime('%b %-d')
   "#{title}<br/> #{author} &\#8211; <span>#{date}</span>"
